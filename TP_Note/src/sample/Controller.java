84579938fbdf09a57e01e7cb7631748f2b39e0ff
AdrienChesneau <adrien78470@gmail.com>
2018-02-22 17:52:48 +0100

    Add files via upload

--- TP_Note/src/sample/Controller.java
+++ TP_Note/src/sample/Controller.java
@@ -8,13 +8,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Shape;
-import javafx.scene.control.ToggleGroup;
-
-import java.awt.*;
-
 
 public class Controller {
 
+    //On ajoute les objets
     @FXML
     private RadioButton selected;
     @FXML
@@ -32,16 +29,17 @@ public class Controller {
     @FXML
     private ColorPicker color;
 
+    //Ajout d'un Shape pour gérer la selection de figure  [NE FONCTIONNE PAS]
     Shape s;
-
-
 
     public Controller(){}
 
     @FXML
     public void initialize(){
+        //On creer une zone graphique sur tout le canvas
         final GraphicsContext gc = canvas.getGraphicsContext2D();
 
+        //Lorsque qu'on clique sur le canvas, on dessine/sectionne une figure selon le radioButton selectionné
         this.canvas.setOnMouseClicked((event) -> {
             gc.setFill(color.getValue());
             gc.setStroke(color.getValue());
@@ -56,15 +54,17 @@ public class Controller {
             }
         });
 
+        //Lorsque on reste appuyé sur une figure, on l'a move
         this.canvas.setOnMouseDragged((event) -> {
-            if (selected.isSelected()) {
-            }});
+            });
 
+        //Si une figure a été selectionné on l'a clone avec un déplacement vers le bas à droite
         this.clone.setOnMouseClicked((event) -> {
             if(selected.isSelected()){
             }
         });
 
+        //Si une figure est selectionné on l'a supprime en l'a rendant opaque
         this.delete.setOnMouseClicked((event) -> {
             if(selected.isSelected()){
                 Paint p = s.getStroke();
